f253db99e354a1fec6970c2977f934ec87de3624
Siddhartha Dutta <siddharthadutta@protonmail.com>
2020-04-09 17:41:12 +0530

    15th Apr

---
+++ 7. Loaders/2. Pass One of DLL/Pass One DLL.cpp
@@ -0,0 +1,57 @@
+/*
+C++ Program to
+implement pass one
+of a direct linking loader
+*/
+#include <iostream>
+#include <array>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    array<string,2> files{"linkin.txt","linkin1.txt"};
+    int csaddr;
+    string line;
+    ofstream outfile("output.txt");
+    cout<<"Enter starting address: ";
+    cin>>csaddr;
+    outfile<<"Starting Address: ";
+    outfile<<csaddr<<endl<<endl;
+    for (int i = 0; i < files.size(); i++)
+    {
+        ifstream inpfile(files[i]);
+        if(inpfile.is_open())
+        {
+            int cslth = 0;
+            while(getline(inpfile,line))
+            {
+                // Splitting Line With ^ Delimiter
+                stringstream check(line);
+                string inter;
+                vector<string> records;
+                while(getline(check, inter, '^'))
+                    records.push_back(inter);
+
+                if(records[0] == "H")
+                {
+                    outfile<<records[1]<<"\t"<<csaddr<<"\t"<<records[3]<<endl;
+                    cslth = stoi(records[3]);
+                }
+
+                if(records[0] == "D")
+                {
+                    for (int i = 1; i < records.size(); i+=2)
+                        outfile<<records[i]<<"\t"<<csaddr+stoi(records[i+1])<<endl;
+                }
+            }
+            csaddr += cslth;
+        }
+        inpfile.close();
+    }
+    outfile.close();
+    return 0;
+}
